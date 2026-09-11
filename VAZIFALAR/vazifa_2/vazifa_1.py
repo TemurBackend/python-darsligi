c1031f725c039ae5f1e2b46d7a1517f45acfd9e3
Temurbek <temurbekofficial@icloud.com>
2026-09-11 22:01:57 +0500

    mashq 3 yangilandi

---
+++ VAZIFALAR/vazifa_2/vazifa_1.py
@@ -0,0 +1,12 @@
+a = 30
+b = 40
+
+c = (a**2 + b**2)**0.5
+per = a + b + c
+s = (a * b) / 2
+sim = per * 3
+
+print("Gipotenuza:", c)
+print("Perimetr:", per)
+print("Yuzasi:", s)
+print("3 qator sim uchun:", sim, "metr")
---
+++ VAZIFALAR/vazifa_2/vazifa_4.py
@@ -0,0 +1,20 @@
+PI = 3.14159
+
+
+r = float(input("Radyusni kiriting:\n>>> "))
+h = float(input("Silindr boyini kiriting:\n>>> "))
+
+v = PI * r ** 2 * h
+s = 2 * PI * r * (r + h)
+
+
+print (f"silindr  hajmi {v}")
+print (f"silindr tola sirti {s}")
+
+
+
+
+
+
+
+
